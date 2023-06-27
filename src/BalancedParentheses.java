5994dcce85700f8ec8348365dfee41a01526b393
Katya Daron <katya_daron@hotmail.com>
2023-06-27 13:39:45 -0400

    Balanced Parentheses

---
+++ src/BalancedParentheses.java
@@ -0,0 +1,33 @@
+//Write a function that takes in a string and returns true or false depending on whether that string has balanced parentheses.
+//A string has balanced parentheses if every opening parenthesis “(“ has a matching closing parenthesis “)” that closes the corresponding opening parenthesis.
+//For the purposes of this problem, you only need to worry about parentheses “(“and “)”, not other opening-and-closing marks, like curly brackets, square brackets, or angle brackets.
+
+import java.util.Stack;
+
+public class BalancedParentheses {
+    public static boolean balancedParentheses(String s) {
+        int numUnclosedOpenParens = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                numUnclosedOpenParens++;
+            } else if (c == ')') {
+                numUnclosedOpenParens--;
+            }
+            
+            if (numUnclosedOpenParens < 0) {
+                return false;
+            }
+        }
+        return numUnclosedOpenParens == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Balanced Parentheses:");
+        System.out.println(balancedParentheses("(1)"));
+        System.out.println(balancedParentheses("((1"));
+        System.out.println(balancedParentheses("(1)()"));
+        System.out.println(balancedParentheses(")("));
+        System.out.println(balancedParentheses(""));
+    }
+}
